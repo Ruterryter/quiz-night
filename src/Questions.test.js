a7ae3ea503098b10dfa47d78bb11848576e2c183
Ruterryter <hanna.ruter@jayway.com>
2020-11-25 08:32:04 +0100

    test for questions.js ok

--- src/Questions.test.js
+++ src/Questions.test.js
@@ -1,10 +1,6 @@
 import { newQuestionsArray } from 'Questions';
 
-//test cases
-//test that the array returns 10 questions
 
 test("Shuffled array", () => {
-    expect(newQuestionsArray).toEqual(
-        expect.arrayContaining([])
-    );
+    expect((newQuestionsArray).length).toEqual(10)
 });
